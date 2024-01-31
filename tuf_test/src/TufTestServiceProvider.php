bb72b14b5101542a6159bca6dcdbbaf0df53340e
renna-sab <sabrinaduflo93@gmail.com>
2024-01-31 09:56:38 -0500

    Add a custom module for testing TUF integration with Package Manager.

---
+++ tuf_test/src/TufTestServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\tuf_test;
+
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\Core\DependencyInjection\ServiceProviderBase;
+use Drupal\package_manager\Validator\PhpTufValidator;
+
+class TufTestServiceProvider extends ServiceProviderBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alter(ContainerBuilder $container) {
+    parent::alter($container);
+
+    $container->getDefinition(PhpTufValidator::class)
+      ->addTag('event_subscriber')
+      ->setArgument('$baseUrl', 'https://drupal:drupal@packages.staging.devdrupal.org/8');
+  }
+
+}
